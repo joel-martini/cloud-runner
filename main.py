cc396df1c5354cc781e9f1c73c5b554a19aa4e2f
joel-martini <maritzjoel@gmail.com>
2026-01-10 00:40:24 +1000

    add player sprite and blit to screen, added display update

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Cloud Runner")
 clock = pygame.time.Clock()
+#player
+player_surf = pygame.transform.scale(pygame.image.load("Assets/phplayer.png").convert_alpha(), (32,32))
+player_rect = player_surf.get_rect(center=(screen_width//2, screen_height//2))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
 
+    screen.blit(player_surf, player_rect)
+    pygame.display.flip()
     clock.tick(60)
