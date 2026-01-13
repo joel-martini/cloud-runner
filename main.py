069a59f0fd4dedd0e8b5afee1bb5ecea869b75df
joel-martini <maritzjoel@gmail.com>
2026-01-13 15:33:09 +1000

    added gravity to player, ground for player to fall on, player jumping and jump cancelling. base player mechanics are complete

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ pygame.display.set_caption("Cloud Runner")
 clock = pygame.time.Clock()
 #player
 player_surf = pygame.transform.scale(pygame.image.load("Assets/phplayer.png").convert_alpha(), (32,32))
-player_rect = player_surf.get_rect(center=(screen_width//2, screen_height - 150))
+player_rect = player_surf.get_rect(midbottom=(screen_width//2, screen_height - 150))
 player_speed = 5
+player_gravity = 0
+#ground
+ground_surf = pygame.image.load("Assets/groundph.png").convert_alpha()
+ground_rect = ground_surf.get_rect(topleft=(0,screen_height- 150))
 
 while True:
     for event in pygame.event.get():
@@ -26,8 +30,22 @@ while True:
     if keys[pygame.K_d]:
         if player_rect.x < screen_width - player_rect.width:
             player_rect.x += player_speed
+    if keys[pygame.K_w]:
+        if player_rect.bottom == ground_rect.top:
+            player_gravity = -15
+    if keys[pygame.K_s]:
+        if player_rect.bottom != ground_rect.top:
+            player_gravity = 11
+
+    player_rect.y += player_gravity
+    player_gravity = player_gravity + 1
+
+    if player_rect.bottom >= ground_rect.top - 1:
+        player_gravity = 0
+        player_rect.bottom = ground_rect.top
 
     screen.fill((0,0,0))
     screen.blit(player_surf, player_rect)
+    screen.blit(ground_surf, ground_rect)
     pygame.display.flip()
     clock.tick(60)
