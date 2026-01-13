2435b8a57eedbac6a72e673721a3624ca74bb934
joel-martini <maritzjoel@gmail.com>
2026-01-13 17:56:59 +1000

    added an array of stationary cloud to make move tnext commit. also commited older uncommited assets

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
 from sys import exit
 
+class Cloud():
+    def __init__(self, x, y, image="Assets/phcloud.png"):
+        self.surf = pygame.transform.scale(pygame.image.load("Assets/phcloud.png").convert_alpha(), (128,16))
+        self.rect = self.surf.get_rect(topleft=(x,y))
+
+    def draw(self, screen):
+        screen.blit(self.surf, self.rect)
+
+
 
 pygame.init()
 
@@ -19,6 +28,8 @@ player_gravity = 0
 ground_surf = pygame.image.load("Assets/groundph.png").convert_alpha()
 ground_rect = ground_surf.get_rect(topleft=(0,screen_height- 150))
 
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -30,21 +41,71 @@ while True:
     if keys[pygame.K_d]:
         if player_rect.x < screen_width - player_rect.width:
             player_rect.x += player_speed
-    if keys[pygame.K_w]:
-        if player_rect.bottom == ground_rect.top:
-            player_gravity = -15
+    if keys[pygame.K_w] and on_platform:
+        player_gravity = -15
     if keys[pygame.K_s]:
         if player_rect.bottom != ground_rect.top:
             player_gravity = 11
 
+    clouds = [
+        Cloud(50, 350),
+        Cloud(50, 250),
+        Cloud(50, 150),
+        Cloud(50, 50),
+        Cloud(250, 350),
+        Cloud(250, 250),
+        Cloud(250, 150),
+        Cloud(250, 50),
+        Cloud(450, 350),
+        Cloud(450, 250),
+        Cloud(450, 150),
+        Cloud(450, 50),
+        Cloud(650, 350),
+        Cloud(650, 250),
+        Cloud(650, 150),
+        Cloud(650, 50),
+    ]
+    on_platform = False
+    prev_bottom = player_rect.bottom
+    prev_top = player_rect.top
+    player_gravity += 1
     player_rect.y += player_gravity
-    player_gravity = player_gravity + 1
 
-    if player_rect.bottom >= ground_rect.top - 1:
-        player_gravity = 0
+
+    for cloud in clouds:
+        if player_rect.colliderect(cloud.rect):
+
+            # landing on top
+            if player_gravity > 0 and prev_bottom <= cloud.rect.top:
+                player_rect.bottom = cloud.rect.top
+                player_gravity = 0
+                on_platform = True
+
+            # hitting underside
+            elif player_gravity < 0 and prev_top >= cloud.rect.bottom:
+                player_rect.top = cloud.rect.bottom
+                player_gravity = 0
+
+            # sides
+            if (player_rect.right > cloud.rect.left and
+                player_rect.left < cloud.rect.left and
+                player_rect.bottom > cloud.rect.top + 5):
+                player_rect.right = cloud.rect.left
+
+            elif (player_rect.left < cloud.rect.right and
+                  player_rect.right > cloud.rect.right and
+                  player_rect.bottom > cloud.rect.top + 5):
+                player_rect.left = cloud.rect.right
+
+    if not on_platform and player_rect.bottom >= ground_rect.top:
         player_rect.bottom = ground_rect.top
+        player_gravity = 0
+        on_platform = True
+
 
     screen.fill((0,0,0))
+    for cloud in clouds:
+        cloud.draw(screen)
     screen.blit(player_surf, player_rect)
     screen.blit(ground_surf, ground_rect)
     pygame.display.flip()
