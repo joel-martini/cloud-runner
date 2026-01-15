b7483a7e72136629a302e2c2617a0ec30af50578
joel-martini <maritzjoel@gmail.com>
2026-01-15 16:10:48 +1000

    redid cloud movement to make it simpler and cleaner, removed locudmove variables and added index to main cloud function

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ import pygame
 from sys import exit
 
 class Cloud():
-    def __init__(self, x, y, image="Assets/phcloud.png"):
-        self.surf = pygame.transform.scale(pygame.image.load("Assets/phcloud.png").convert_alpha(), (128,16))
+    def __init__(self, x, y, speed = 1, image="Assets/phcloud.png", index = 1):
+        self.surf = pygame.transform.scale(
+            pygame.image.load("Assets/phcloud.png").convert_alpha(),
+            (128,16))
         self.rect = self.surf.get_rect(topleft=(x,y))
+        self.speed = speed
+        self.index = index
+
+    def update(self):
+        if self.index == 1:
+            self.rect.x += self.speed
+            if self.rect.left > screen_width:
+                self.rect.right = 0
+        if self.index == 2:
+            self.rect.x -= self.speed
+            if self.rect.right < 0:
+                self.rect.left = screen_width
+
 
     def draw(self, screen):
         screen.blit(self.surf, self.rect)
@@ -24,15 +39,17 @@ player_surf = pygame.transform.scale(pygame.image.load("Assets/phplayer.png").co
 player_rect = player_surf.get_rect(midbottom=(screen_width//2, screen_height - 150))
 player_speed = 5
 player_gravity = 0
+on_platform = False
 #ground
 ground_surf = pygame.image.load("Assets/groundph.png").convert_alpha()
 ground_rect = ground_surf.get_rect(topleft=(0,screen_height- 150))
-#clouds
-cloudmovex = 0
-cloudmoveindex = 1
-cloudmovespeed = 0.5
 
-
+clouds = [
+    Cloud(50, 350, index = 1), Cloud(50, 250, index = 2), Cloud(50, 150, index = 1), Cloud(50, 50, index =2),
+    Cloud(250, 350, index = 1), Cloud(250, 250, index = 2), Cloud(250, 150, index = 1), Cloud(250, 50, index = 2),
+    Cloud(450, 350, index = 1), Cloud(450, 250, index = 2), Cloud(450, 150, index = 1), Cloud( 450, 50, index = 2),
+    Cloud(650, 350, index = 1), Cloud(650, 250, index = 2), Cloud(650, 150, index = 1), Cloud( 650, 50, index = 2),
+]
 
 while True:
     for event in pygame.event.get():
@@ -51,33 +68,12 @@ while True:
         if player_rect.bottom != ground_rect.top:
             player_gravity = 11
 
-    if cloudmoveindex == 1:
-        cloudmovex += cloudmovespeed
-    elif cloudmoveindex == 2:
-        cloudmovex -= cloudmovespeed
-    if cloudmovex == 70:
-        cloudmoveindex = 2
-    elif cloudmovex == 0:
-        cloudmoveindex = 1
 
-    clouds = [
-        Cloud(cloudmovex + 50, 350),
-        Cloud(cloudmovex + 50, 250),
-        Cloud(cloudmovex + 50, 150),
-        Cloud(cloudmovex + 50, 50),
-        Cloud(cloudmovex + 250, 350),
-        Cloud(cloudmovex + 250, 250),
-        Cloud(cloudmovex + 250, 150),
-        Cloud(cloudmovex + 250, 50),
-        Cloud(cloudmovex + 450, 350),
-        Cloud(cloudmovex + 450, 250),
-        Cloud(cloudmovex + 450, 150),
-        Cloud(cloudmovex + 450, 50),
-        Cloud(cloudmovex + 650, 350),
-        Cloud(cloudmovex + 650, 250),
-        Cloud(cloudmovex + 650, 150),
-        Cloud(cloudmovex + 650, 50),
-    ]
+
+
+    for cloud in clouds:
+        cloud.update()
+
     on_platform = False
     prev_bottom = player_rect.bottom
     prev_top = player_rect.top
